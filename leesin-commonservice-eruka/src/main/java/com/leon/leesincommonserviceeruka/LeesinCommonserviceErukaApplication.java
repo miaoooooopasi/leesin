30e1c30f4db6aae7b99ba60fd17ea9f50058aa89
miaopasi <1429169422@qq.com>
2020-07-01 19:09:34 +0800

    服务注册中心

--- leesin-commonservice-eruka/src/main/java/com/leon/leesincommonserviceeruka/LeesinCommonserviceErukaApplication.java
+++ leesin-commonservice-eruka/src/main/java/com/leon/leesincommonserviceeruka/LeesinCommonserviceErukaApplication.java
@@ -1,6 +1,7 @@
 package com.leon.leesincommonserviceeruka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
